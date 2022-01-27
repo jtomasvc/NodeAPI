9b604e63f2bdf5b02f20f4a7308570700b61d0db
jtomasvc <julian_95@hotmail.com>
2022-01-27 16:18:23 -0500

    algunos ajustes antes del deployment

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.DB_URL, {
 //Crear servidor 
 const app = express();
 
+//Carpeta publica
+app.use(express.static('uploads'))
+
 //Habilitar bodyParser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -39,9 +42,6 @@ app.use(cors(corsOptions))
 
 // Rutas de la App
 app.use('/', routes())
-
-//Carpeta publica
-app.use(express.static('uploads'))
 
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT    || 5000
